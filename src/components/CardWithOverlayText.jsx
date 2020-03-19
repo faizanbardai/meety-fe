be6db0ac46cc3914a6f659f38673dc2b0de32037
vennhost <samogconsult@gmail.com>
2020-03-19 10:16:26 +0100

    share button

--- src/components/CardWithOverlayText.jsx
+++ src/components/CardWithOverlayText.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+import {FacebookShareButton, TwitterShareButton, WhatsappShareButton} from "react-share";
+
+import {FacebookIcon, TwitterIcon, WhatsappIcon} from "react-share";
+
+
 export default function CardWithOverlayText({ hotItem }) {
   const { image, title, dateAndTime, participants } = hotItem;
   return (
@@ -15,7 +20,11 @@ export default function CardWithOverlayText({ hotItem }) {
         <h3>
           {dateAndTime}&nbsp;&nbsp; / &nbsp;&nbsp;{participants} Participants
         </h3>
+        <FacebookShareButton url={shareUrl}> <FacebookIcon size={32} round={true} /> </FacebookShareButton>  <TwitterShareButton url={shareUrl}><TwitterIcon size={32} round={true} /></TwitterShareButton>  <WhatsappShareButton url={shareUrl}><WhatsappIcon size={32} round={true} /></WhatsappShareButton> 
       </div>
     </div>
   );
 }
+
+const shareUrl = String(window.location);
+

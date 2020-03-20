ef160d6133f4e3d7422bd867a023c44d6f486a75
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-03-20 23:51:27 +0100

    Updates

---
+++ precache-manifest.5ba95305061fac8ba4c5c2871d3470fa.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8df8f9f4f3d9820b52f4fc4f7238b5a4",
+    "url": "/meety-fe/index.html"
+  },
+  {
+    "revision": "a9a85ba639dbb96fc595",
+    "url": "/meety-fe/static/css/main.947a2747.chunk.css"
+  },
+  {
+    "revision": "717a99498d4ae7a1943f",
+    "url": "/meety-fe/static/js/2.6272b2c0.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/meety-fe/static/js/2.6272b2c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9a85ba639dbb96fc595",
+    "url": "/meety-fe/static/js/main.a53912d9.chunk.js"
+  },
+  {
+    "revision": "819935bd47303bea8c76",
+    "url": "/meety-fe/static/js/runtime-main.39c1f6f9.js"
+  },
+  {
+    "revision": "3f85496dd6673a99ba343907fa8afc9b",
+    "url": "/meety-fe/static/media/event.3f85496d.png"
+  },
+  {
+    "revision": "8926dda516ebae3fe8d2c51835d20a56",
+    "url": "/meety-fe/static/media/home.8926dda5.png"
+  },
+  {
+    "revision": "77fe2c9d1377558b4e1861ed99457a4c",
+    "url": "/meety-fe/static/media/hottest.77fe2c9d.png"
+  },
+  {
+    "revision": "0fdd10430fd135e2bbe720c905db58e3",
+    "url": "/meety-fe/static/media/join-event.0fdd1043.png"
+  },
+  {
+    "revision": "ab1941f583b1996f33183947c1cb2671",
+    "url": "/meety-fe/static/media/new-event.ab1941f5.png"
+  },
+  {
+    "revision": "ea14f3fe691771e2b947d88afdea319d",
+    "url": "/meety-fe/static/media/profile.ea14f3fe.png"
+  },
+  {
+    "revision": "bc6e7b451701b49150e66ad17359f684",
+    "url": "/meety-fe/static/media/share-event.bc6e7b45.png"
+  }
+]);

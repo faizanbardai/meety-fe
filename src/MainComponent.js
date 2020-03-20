985956c02f7d4745ca25e1dc94a10772b89d26f3
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-03-20 16:07:14 +0100

    authenticating user on first visit

--- src/MainComponent.js
+++ src/MainComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "./index.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
@@ -6,27 +6,62 @@ import PageNotFound from "./pages/PageNotFound";
 import Introduction from "./pages/Introduction";
 import Event from "./pages/Event";
 import Profile from "./pages/Profile";
+import { api_refreshToken } from "../src/apis/users";
 
-export default function MainComponent() {
-  return (
-    <div>
-      <Router>
-        <Switch>
-          <Route path="/" exact>
-            <Introduction />
-          </Route>
-          <Route path="/home" exact>
-            <Home />
-          </Route>
-          <Route path="/event" exact>
-            <Event />
-          </Route>
-          <Route path="/profile" exact>
-            <Profile />
-          </Route>
-          <Route component={PageNotFound} />
-        </Switch>
-      </Router>
-    </div>
-  );
+export default class MainComponent extends Component {
+  state = {
+    isAuthenticated: true
+  };
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route path="/" exact>
+              <Introduction />
+            </Route>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/event">
+              <Event />
+            </Route>
+            <Route path="/profile/:_id">
+              {this.state.isAuthenticated ? (
+                <Profile authorised={this.state.authorised} />
+              ) : (
+                <Home />
+              )}
+            </Route>
+            <Route component={PageNotFound} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+  componentDidMount = async () => {
+    const accessToken = localStorage.getItem("accessToken");
+    try {
+      let response = await api_refreshToken(accessToken);
+      switch (response.status) {
+        case 200:
+          // OK
+          response = await response.json();
+          this.setState({ user: response.user });
+          localStorage.setItem("accessToken", response.access_token);
+          break;
+        case 401:
+          // unauthorized
+          localStorage.removeItem("accessToken");
+          alert("You are unauthorized or your token has expired");
+          break;
+        default:
+          alert("Some error");
+          break;
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
 }

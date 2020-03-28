43121c3e1f373e232a54b1304183f75cb02a2729
vennhost <samogconsult@gmail.com>
2020-03-28 14:09:27 +0100

    fix conflict

--- src/components/CardWithOverlayText.jsx
+++ src/components/CardWithOverlayText.jsx
@@ -2,18 +2,8 @@ import React from "react";
 import Moment from "react-moment";
 import { withRouter, Link } from "react-router-dom";
 
-<<<<<<< HEAD
-import {FacebookShareButton, TwitterShareButton, WhatsappShareButton} from "react-share";
-
-import {FacebookIcon, TwitterIcon, WhatsappIcon} from "react-share";
-
-
-export default function CardWithOverlayText({ hotItem }) {
-  const { image, title, dateAndTime, participants } = hotItem;
-=======
 function CardWithOverlayText(props) {
   const { picture, name, schedule, participantsLength } = props.event;
->>>>>>> master
   return (
     <Link
       to={{
@@ -34,15 +24,10 @@ function CardWithOverlayText(props) {
           <Moment fromNow>{schedule}</Moment>&nbsp;&nbsp; / &nbsp;&nbsp;
           {participantsLength ? participantsLength : "0"} Participants
         </h3>
-        <FacebookShareButton url={shareUrl}> <FacebookIcon size={32} round={true} /> </FacebookShareButton>  <TwitterShareButton url={shareUrl}><TwitterIcon size={32} round={true} /></TwitterShareButton>  <WhatsappShareButton url={shareUrl}><WhatsappIcon size={32} round={true} /></WhatsappShareButton> 
+         
       </div>
     </Link>
   );
 }
 
-<<<<<<< HEAD
-const shareUrl = String(window.location);
-
-=======
 export default withRouter(CardWithOverlayText);
->>>>>>> master

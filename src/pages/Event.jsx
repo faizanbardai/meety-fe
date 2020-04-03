d3871b92e0907071b3bb79e08ea1dd31cc27d206
zabed <zabedalam777@gmail.com>
2020-04-03 15:44:37 +0200

    added api for participants

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { api_getEventByID } from "../apis/events";
+import {
+  api_getEventByID,
+  api_joinEvent,
+  api_leaveEvent
+} from "../apis/events";
+import {api_followers,api_unfollow} from "../apis/users"
 import BurgerMenu from "../components/BurgerMenu";
 import Header from "../components/Header";
 import CardWithOverlayText from "../components/CardWithOverlayText";
@@ -10,8 +15,81 @@ import MiniProfileCard from "../components/MiniProfileCard";
 const mapStateToProps = state => ({ ...state });
 
 class Event extends Component {
-  handleFollowButton = () => {
+  state = {
+    participants: "",
+    followers:""
+    
+  };
+  //here i go
+  handleJoinButton = async () => {
+    console.log("I'm join button");
+    try {
+      const accessToken = this.props.accessToken;
+      const res = await api_joinEvent(accessToken, this.state.event._id);
+      if (res.ok) {
+        this.setState({
+          event: {
+            ...this.state.event,
+            participants: [...this.state.event.participants, this.props.user]
+          }
+          
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  handleCancelButton = async () => {
+    console.log("I'm cancel button");
+    try {
+      const accessToken = this.props.accessToken;
+      const res = await api_leaveEvent(accessToken, this.state.event._id);
+      if (res.ok) {
+        this.setState({
+          event: {
+            ...this.state.event,
+            participants: this.state.event.participants.filter(x => x._id !== this.props.user._id)
+          }
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  handleFollowButton =async () => {
     console.log("I'm follow button");
+    try{
+      const accessToken=this.props.accessToken
+      const res=await api_followers(accessToken,this.state.event.host[0]._id)
+      console.log(res)
+      if(res.ok){
+        this.setState({
+          followers:[...this.state.followers,this.user._id]
+        })
+      }
+    }
+    catch(error){
+      console.log(error)
+    }
+  };
+
+  handleUnFollowButton =async () => {
+    console.log("I'm follow button");
+    try{
+      const accessToken=this.props.accessToken
+      const res=await api_unfollow(accessToken,this.state.event.host[0]._id)
+      console.log(res)
+      if(res.ok){
+        this.setState({
+          followers:[...this.state.followers,this.user._id]
+        })
+      }
+    }
+    catch(error){
+      console.log(error)
+    }
   };
   state = {};
   render() {
@@ -68,12 +146,37 @@ class Event extends Component {
                   >
                     <button className="button">Edit</button>
                   </Link>
-                ) : (
+                ) :
+                  
+                (
                   <button className="button" onClick={this.handleFollowButton}>
                     Follow
                   </button>
+                  
+                  
+  )}
+  {/* //here i go */}
+  {/* {!this.state.followers.includes(this.props.user._id)?
+  <button className="button" onClick={this.handleFollowButton}>
+  Follow
+</button>:<button className="button" onClick={this.handleUnFollowButton}>
+                   Unfollow
+                  </button>}
+                 */}
+                
+                {!this.state.event.participants.map(x => x._id).includes(
+                  this.props.user._id
+                ) ? (
+                  <button className="button" onClick={this.handleJoinButton}>
+                    Join
+                  </button>
+                ) : (
+                  <button className="button" onClick={this.handleCancelButton}>
+                    Cancel
+                  </button>
                 )}
               </div>
+              
             </div>
             <div className="about-event">
               <h1>Details</h1>
@@ -84,7 +187,11 @@ class Event extends Component {
             </div>
             <div className="hosts">
               {this.state.event.participants.map(participant => (
-                <MiniProfileCard key={participant._id} item={participant} />
+                 <MiniProfileCard
+                  key={participant._id}
+                  // item={{name:this.props.user.name,picture:this.props.user.picture}}
+                    item={participant} 
+                 ></MiniProfileCard>
               ))}
             </div>
           </div>
@@ -103,5 +210,6 @@ class Event extends Component {
       this.setState({ event });
     }
   };
+  
 }
 export default connect(mapStateToProps, null)(withRouter(Event));

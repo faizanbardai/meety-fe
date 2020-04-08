92fae5651ae457b6109ac59458fc76c6d951ff97
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-04-08 21:58:44 +0200

    Header Fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import burgerMenu from "../../src/img/burger.png";
 import { Link } from "react-router-dom";
+import "./Header.css";
 
 export default function Header() {
   return (
     <div className="head">
-      <div id="menu">
+      <div className="container">
         <img
-          id="burguer"
+          className="mx-2 my-3 position-fixed"
           src={burgerMenu}
           alt="menu"
           width="35px"
           height="35px"
         />
+        <div className="w-100 text-center">
+          <Link to="/home" className="logo">
+            Meety
+          </Link>
+        </div>
       </div>
-      <Link to="/home" className="logo">
-        <div className="logo">Meety</div>
-      </Link>
     </div>
   );
 }

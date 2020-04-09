edceffda58b730c499b3ca07bd597fff6b37f6cb
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-04-09 12:31:45 +0200

    Burger Menu before and after login

---
+++ src/components/MenuAfterLogin.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { saveUser, saveAccessToken } from "../action";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+
+const mapStateToProps = (state) => ({ ...state });
+const mapDispatchToProps = (dispatch) => ({
+  saveUser: (user) => dispatch(saveUser(user)),
+  saveAccessToken: (accessToken) => dispatch(saveAccessToken(accessToken)),
+});
+
+const MenuAfterLogin = (props) => {
+  return (
+    <div
+      style={{ zIndex: 1 }}
+      className="w-100 h-100 position-fixed bg-dark text-white"
+    >
+      <div className="container">
+        <div className="d-flex">
+          <FontAwesomeIcon
+            icon={faTimes}
+            size="2x"
+            className="mx-2 my-3 "
+            onClick={() => {
+              props.setShowMenu(false);
+            }}
+          />
+          <div className="ml-auto">
+            <button
+              type="button"
+              className="my-2 btn btn-outline-light"
+              onClick={() => {
+                localStorage.removeItem("accessToken");
+                props.saveUser(null);
+                props.saveAccessToken(null);
+                props.history.push("/home");
+              }}
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+        <div>
+          <div className="row">
+            <img
+              src={props.user.picture}
+              alt={props.user.name}
+              className="rounded-circle col-6 col-sm-4 col-md-3"
+            />
+
+            <div className="col-12 col-sm-8 col-md-9">
+              <h1 className="display-4 text-white">{props.user.name}</h1>
+              <p>{props.user.aboutMe}</p>
+            </div>
+          </div>
+          <div className="row d-flex justify-content-center">
+            <button type="button" className="mx-2 my-2 btn btn-outline-light">
+              My Events
+            </button>
+            <button type="button" className="mx-2 my-2 btn btn-outline-light">
+              Create Event{" "}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withRouter(MenuAfterLogin));
---
+++ src/components/MenuBeforeLogin.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+
+function MenuBeforeLogin(props) {
+  return (
+    <div
+      style={{ zIndex: 1 }}
+      className="w-100 h-100 position-fixed bg-dark text-white"
+    >
+      <div className="container">
+        <div className="d-flex">
+          <FontAwesomeIcon
+            icon={faTimes}
+            size="2x"
+            className="mx-2 my-3 "
+            onClick={() => {
+              props.setShowMenu(false);
+            }}
+          />
+          <div className="ml-auto">
+            <button
+              onClick={() => {
+                props.setShowMenu(false);
+                props.history.push("/login");
+              }}
+              type="button"
+              className="mt-3 mr-2 btn btn-primary rounded-pill"
+            >
+              Log In
+            </button>
+            <button type="button" className="mt-3 btn btn-primary rounded-pill">
+              Create Account
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default withRouter(MenuBeforeLogin);

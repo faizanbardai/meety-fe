6b88b30d1baec03dde84a7c70c31494753c6f279
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-04-09 15:24:31 +0200

    Card fixes

--- src/components/CardWithOverlayText.jsx
+++ src/components/CardWithOverlayText.jsx
@@ -1,29 +1,36 @@
 import React from "react";
 import Moment from "react-moment";
 import { withRouter, Link } from "react-router-dom";
-import {} from "reactstrap"
+import "./CardWithOverlayText.css";
 
 function CardWithOverlayText(props) {
   const { picture, name, schedule, participantsLength, _id } = props.event;
   return (
-    
-    <Link
-      to={"/event/" + _id}
-      className="card-big shadow"
-      style={{
-        backgroundImage: `url(${picture})`,
-        backgroundSize: "cover",
-      }}
-    >
-      <div className="card-title">
-        <h2>{name}</h2>
-        <h3>
-          <Moment fromNow>{schedule}</Moment>&nbsp;&nbsp; / &nbsp;&nbsp;
-          {participantsLength ? participantsLength : "0"} Participants
-        </h3>
+    <div className="col-12 col-sm-6 col-md-4 mb-3">
+      <div
+        className="card deep-shadow"
+        style={{
+          backgroundImage: `url(${picture})`,
+          backgroundSize: "cover",
+          borderRadius: "10px",
+          border: "none",
+        }}
+      >
+        <Link to={"/event/" + _id}>
+          <div
+            style={{ height: "300px" }}
+            className="text-white px-3 d-flex flex-column"
+          >
+            <div className="mb-auto"></div>
+            <h6 className="py-2">{name}</h6>
+            <div>
+              <Moment fromNow>{schedule}</Moment>&nbsp;&nbsp; / &nbsp;&nbsp;
+              {participantsLength ? participantsLength : "0"} Participants
+            </div>
+          </div>
+        </Link>
       </div>
-    </Link>
-    
+    </div>
   );
 }
 

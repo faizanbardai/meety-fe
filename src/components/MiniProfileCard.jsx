aacfa67b65f7d98e57fbc74c6472a430bccc5076
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-04-09 21:37:49 +0200

    minor changes

--- src/components/MiniProfileCard.jsx
+++ src/components/MiniProfileCard.jsx
@@ -5,13 +5,18 @@ function MiniProfileCard(props) {
   const { picture, name, _id } = props.item;
   return (
     <div
-      className="host-minicard"
+      className="card m-2"
+      style={{ width: "150px", border: "none" }}
       onClick={() => {
         props.history.push("/profile/" + _id);
       }}
     >
-      <div className="img" style={{ backgroundImage: `url(${picture})` }}></div>
-      <div className="name">{name}</div>
+      <div className="px-4">
+        <img className="card-img-top rounded-circle" src={picture} alt={name} />
+      </div>
+      <div className="card-body text-center">
+        <h5 className="card-title">{name}</h5>
+      </div>
     </div>
   );
 }

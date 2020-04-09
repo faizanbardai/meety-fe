c098c4c98c65ffff100d4627ac1e7318cab8490f
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-04-09 17:03:26 +0200

    Added max-width

--- src/components/CardWithOverlayText.jsx
+++ src/components/CardWithOverlayText.jsx
@@ -6,30 +6,29 @@ import "./CardWithOverlayText.css";
 function CardWithOverlayText(props) {
   const { picture, name, schedule, participantsLength, _id } = props.event;
   return (
-    <div className="col-12 col-sm-6 col-md-4 mb-3">
-      <div
-        className="card deep-shadow"
-        style={{
-          backgroundImage: `url(${picture})`,
-          backgroundSize: "cover",
-          borderRadius: "10px",
-          border: "none",
-        }}
-      >
-        <Link to={"/event/" + _id}>
-          <div
-            style={{ height: "300px" }}
-            className="text-white px-3 d-flex flex-column"
-          >
-            <div className="mb-auto"></div>
-            <h6 className="py-2">{name}</h6>
-            <div>
-              <Moment fromNow>{schedule}</Moment>&nbsp;&nbsp; / &nbsp;&nbsp;
-              {participantsLength ? participantsLength : "0"} Participants
-            </div>
+    <div
+      className="card deep-shadow"
+      style={{
+        backgroundImage: `url(${picture})`,
+        backgroundSize: "cover",
+        borderRadius: "10px",
+        border: "none",
+        maxWidth: "600px",
+      }}
+    >
+      <Link to={"/event/" + _id}>
+        <div
+          style={{ height: "300px" }}
+          className="text-white px-3 d-flex flex-column"
+        >
+          <div className="mb-auto"></div>
+          <h6 className="py-2">{name}</h6>
+          <div>
+            <Moment fromNow>{schedule}</Moment>&nbsp;&nbsp; / &nbsp;&nbsp;
+            {participantsLength ? participantsLength : "0"} Participants
           </div>
-        </Link>
-      </div>
+        </div>
+      </Link>
     </div>
   );
 }
